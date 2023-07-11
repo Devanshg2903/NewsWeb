cb5f8d0fe91ddb179b358311c18aa995c50c5aa5
Devansh Gautam <devansh.gautam290302@gmail.com>
2023-07-11 23:10:57 +0530

    api key updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "16ddbc77660a4dee8feb9d793af50799";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
